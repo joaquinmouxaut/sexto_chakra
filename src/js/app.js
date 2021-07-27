9e460a025a3fd46742752b408d37bc853bc51ab4
Joaquin Mouxaut <joaquinmouxaut@gmail.com>
2021-07-27 18:21:05 -0300

    trabajos en barra de navegacion

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     scrollNav();
+    eventListeners();
 } )
+
+function eventListeners() {
+    const mobilMenu = document.querySelector( '.mobil-menu' );
+    mobilMenu.addEventListener( 'click', navegacionResponsive );
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector( '.navegacion' );
+    navegacion.classList.toggle( 'desplegar' );
+}
 
 function scrollNav() {
     const enlaces = document.querySelectorAll( '.navegacion a' );
